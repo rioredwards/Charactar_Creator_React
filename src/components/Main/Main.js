0f4f92a0ceed4690e76c2212e295b808acccf358
Rio Edwards <rioredwards@gmail.com>
2023-01-11 17:52:39 -0800

    Add Main

---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import './Main.css';
+
+export default function Main() {
+  return (
+    <>
+      <main>
+        <div className="TEMP-editor-container" />
+        <div className="TEMP-preview-container" />
+      </main>
+    </>
+  );
+}
